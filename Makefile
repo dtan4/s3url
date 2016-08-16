479032c76f3354131f679c4b059be2c4a926719a
Daisuke Fujita <dtanshi45@gmail.com>
2016-08-16 18:53:31 +0900

    Implement entire command

--- Makefile
+++ Makefile
@@ -1,13 +1,38 @@
 BINARY := s3psurl
 LDFLAGS := -ldflags="-s -w"
-
 SOURCES := $(shell find . -name "*.go")
+
+GLIDE_VERSION := 0.11.1
 
 .DEFAULT_GOAL := bin/$(BINARY)
 
-bin/$(BINARY): $(SOURCES)
+bin/$(BINARY): deps $(SOURCES)
 	go build $(LDFLAGS) -o bin/$(BINARY)
 
 .PHONY: clean
 clean:
 	rm -rf bin/*
+	rm -rf vendor/*
+
+.PHONY: deps
+deps: glide
+	./glide install
+
+glide:
+ifeq ($(shell uname),Darwin)
+	curl -fL https://github.com/Masterminds/glide/releases/download/v$(GLIDE_VERSION)/glide-v$(GLIDE_VERSION)-darwin-amd64.zip -o glide.zip
+	unzip glide.zip
+	mv ./darwin-amd64/glide glide
+	rm -fr ./darwin-amd64
+	rm ./glide.zip
+else
+	curl -fL https://github.com/Masterminds/glide/releases/download/v$(GLIDE_VERSION)/glide-v$(GLIDE_VERSION)-linux-amd64.zip -o glide.zip
+	unzip glide.zip
+	mv ./linux-amd64/glide glide
+	rm -fr ./linux-amd64
+	rm ./glide.zip
+endif
+
+.PHONY: update-deps
+update-deps: glide
+	./glide update
--- main.go
+++ main.go
@@ -1,9 +1,66 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	defaultDuration = 5
 )
 
 func main() {
-	fmt.Println("hello")
+	var (
+		bucket   string
+		duration int64
+		key      string
+	)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage of %s:
+   %s [OPTIONS]
+
+Options:
+`, os.Args[0], os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&bucket, "bucket", "", "Bucket name")
+	flag.StringVar(&bucket, "b", "", "Bucket name")
+	flag.Int64Var(&duration, "duration", defaultDuration, "Valid duration in minutes")
+	flag.Int64Var(&duration, "d", defaultDuration, "Valid duration in minutes")
+	flag.StringVar(&key, "key", "", "Object key")
+	flag.StringVar(&key, "k", "", "Object key")
+
+	flag.Parse()
+
+	if bucket == "" {
+		fmt.Fprintln(os.Stderr, "Bucket name is required.")
+		os.Exit(1)
+	}
+
+	if key == "" {
+		fmt.Fprintln(os.Stderr, "Object key is required.")
+		os.Exit(1)
+	}
+
+	svc := s3.New(session.New(), &aws.Config{})
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	url, err := req.Presign(time.Duration(duration) * time.Minute)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println(url)
 }
