29d4cc4e3f3757daea4c09d2accd660f3db1a249
Daisuke Fujita <dtanshi45@gmail.com>
2016-11-30 00:50:48 +0900

    Implement UploadToS3

--- s3/s3.go
+++ s3/s3.go
@@ -1,6 +1,10 @@
 package s3
 
 import (
+	"bytes"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 )
 
@@ -14,4 +18,18 @@ func NewClient(api s3iface.S3API) *Client {
 	return &Client{
 		api: api,
 	}
+}
+
+// UploadToS3 uploads local file to the specified S3 location
+func (c *Client) UploadToS3(bucket, key string, body []byte) error {
+	_, err := c.api.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(body),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- s3/s3_test.go
+++ s3/s3_test.go
@@ -1,8 +1,11 @@
 package s3
 
 import (
+	"bytes"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/dtan4/s3url/awsmock"
 	"github.com/golang/mock/gomock"
 )
@@ -16,5 +19,28 @@ func TestNewClient(t *testing.T) {
 
 	if client.api != s3mock {
 		t.Error("api does not match.")
+	}
+}
+
+func TestUploadToS3(t *testing.T) {
+	bucket := "bucket"
+	key := "key"
+	body := []byte("filebody")
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	s3mock := awsmock.NewMockS3API(ctrl)
+	s3mock.EXPECT().PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader([]byte(body)),
+	}).Return(&s3.PutObjectOutput{}, nil)
+	client := &Client{
+		api: s3mock,
+	}
+
+	if err := client.UploadToS3(bucket, key, body); err != nil {
+		t.Errorf("Error should not be raised. error: %s", err)
 	}
 }
